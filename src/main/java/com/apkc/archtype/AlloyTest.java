0e7e34ee1b28162fab9ce1a434370df5ebad05f3
Kristoffer Cobley <kcobley@gmail.com>
2012-11-16 11:58:11 +0100

    first alloy test class

---
+++ src/main/java/com/apkc/archtype/AlloyTest.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.apkc.archtype;
+
+/**
+ *
+ * @author mundane
+ */
+public class AlloyTest {
+    
+}
